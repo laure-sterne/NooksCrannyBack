d2bd43b8d52615c604632b3e0b5cc8870e7a1efe
celinelouaintier <celine.louaintier@gmail.com>
2022-03-07 16:20:07 +0100

    data base correction

--- data/create_tables.sql
+++ data/create_tables.sql
@@ -1,17 +1,18 @@
 BEGIN;
 
 DROP TABLE IF EXISTS "utilisateur", "meuble";
+
 CREATE TABLE IF NOT EXISTS "utilisateur"(
     "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
     "mail" TEXT NOT NULL,
     "password" TEXT NOT NULL, 
     "pseudo" TEXT NOT NULL, 
-    "statut" TEXT NOT NULL DEFAULT "acheteur"
+    "statut" TEXT NOT NULL DEFAULT 'acheteur'
     -- on donnera le statut d'admin à la propriétaire à la création, tous les autres seront juste acheteurs 
 );
 CREATE TABLE IF NOT EXISTS "meuble"(
     "id" INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
-    "photo" TEXT NOT NULL DEFAULT "public\placeholder.jpeg",
+    "photo" TEXT NOT NULL DEFAULT 'public\placeholder.jpeg',
     "type" TEXT NOT NULL,
     "couleur" TEXT NOT NULL,
     "matiere" TEXT NOT NULL,
@@ -19,7 +20,7 @@ CREATE TABLE IF NOT EXISTS "meuble"(
     "longueur" INT,
     "hauteur" INT,
     "prix" INT NOT NULL,
-    "statut" TEXT NOT NULL DEFAULT "disponible"
+    "statut" TEXT NOT NULL DEFAULT 'disponible'
 );
 
 COMMIT;
