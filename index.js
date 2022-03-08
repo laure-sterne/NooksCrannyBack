fd6e9d158abbbb5d8957fdc3e2d34ec535615421
CelineBarbe <laloutrecosmique@hotmail.fr>
2022-03-08 10:33:35 +0100

    comment and pimp server express

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+//on appelle le fichier .env qui contient les variables d'environnement et doit rester secret. Il est à créer en prenant le .env.example pour modèle
+require('dotenv').config();
 const express = require('express');
-
+//création d'une instance d'express
 const app = express();
 
 app.use(express.json());
 
+//première route basique de test de mise en place
 app.get('/', function (req, res) {
     res.send('hello world')
   })
-  
-app.listen(3000);
+
+// vérification dans la console que le serveur tourne et sur quel port.
+app.listen(process.env.PORT || 4000, () => {
+    console.log('Server running on :', process.env.PORT);
+});
