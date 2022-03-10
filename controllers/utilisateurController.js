4de47bfdb17f0abfde97f97792f7ad55f86acd36
Isciane <91957387+IscianeN@users.noreply.github.com>
2022-03-10 11:08:07 +0100

    test architecture router/controller : it works

---
+++ controllers/utilisateurController.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+
+ //username and password test
+ const myusername = 'user1'
+ const mypassword = 'mypassword'
+//identification user 
+exports.identifyUser =(req,res) =>{
+
+
+    if(req.body.username == myusername && req.body.password == mypassword){
+        req.session.userid=req.body.username;
+        console.log(req.session)
+        res.send(`Hey there, welcome ${req.session.userid} <a href=\'/logout'>click to logout</a>`);
+    }
+    else{
+        res.send('Invalid username or password');
+    }
+  }
+
+
+  
+// création d'un utilisateur 
+ exports.createUser = async(req,res) =>{
+
+    // faire l'échappemment 
+    var mail = connection.escape(req.body.mail)
+    var pseudo = connection.escape(req.body.pseudo)
+    var mdp = connection.escape(req.body.mdp)
+    await bcrypt.hash(mdp, 10).then(hash => {mdp = hash})
+  
+    // check if user exist 
+    connection.query({
+      sql: `SELECT mail FROM utilisateur WHERE mail = ${mail} `,
+      timeout: 40000}, function (err, result) {
+        if(err){throw err};
+        if(result[0]){res.send("email déjà inscrit")
+        console.log(result) } 
+        else {
+  
+    connection.query({
+    sql: `INSERT INTO utilisateur (id, mail, mdp, pseudo) VALUES (NULL, ${mail}, '${mdp}', ${pseudo})`, 
+    timeout: 40000}, function (err, result) {
+      if(err) throw err;
+      console.log(result)
+    })
+    res.send("utilisateur créé")
+    }
+  }
+  )
+  }
+
+//  page d'accueil 
+
+  exports.welcome =(req,res) => {
+      console.log('bonjour fonctionne')
+    if(req.session.userid){
+        res.send("Welcome User <a href=\'/logout'>click to logout</a>");
+    }else
+    res.sendFile('./index.html',{root:__dirname})
+  };
+
+  //page après connexion 
+  exports.connected =(req,res)=>{
+    res.send(`Hey there, welcome ${req.session.userid} <a href=\'/logout'>click to logout</a>`);
+  }
+
+  // déconnexion
+  exports.logOut = (req,res) => {
+    req.session.destroy();
+    res.redirect('/');
+  };
---
+++ routeur.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router =express.Router();
+const controlMeuble = require('./controllers/meubleController.js')
+const controlUser = require('./controllers/utilisateurController.js')
+
+//Route post user
+router.post('/user',controlUser.identifyUser)
+router.post('/createuser',controlUser.createUser)
+
+//Route get user
+router.get('/',controlUser.welcome)
+router.get('/user',controlUser.connected)
+router.get('/logout',controlUser.logOut)
+
+//route get furniture
+
+
+module.exports = router
+
+
+
