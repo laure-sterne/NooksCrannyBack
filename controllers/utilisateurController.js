b316ee0f152487c2b44ae07d9fda8a52f9a93c85
Camille <folcamille@yahoo.fr>
2022-03-17 16:45:29 +0100

    Cryptage du mot de passe

--- controllers/utilisateurController.js
+++ controllers/utilisateurController.js
@@ -1,11 +1,7 @@
 const bcrypt = require('bcrypt')
 const connection = require('../connectionToBdd')
 
-//username and password test 
-//const myEmail = 'user1@u.fr'
-//const myPassword = 'mypassword'
-
-identifyUser = (req,result,res,myEmail,myPassword) => {
+identifyUser = (req,result,myEmail,myPassword) => {
   console.log("Tentative de connexion");
   console.log("je suis un résultat", result);
   if(connection.escape(Object.values(result[0])[0])== myEmail && (connection.escape(Object.values(result[0])[1]) == myPassword)){
@@ -15,36 +11,28 @@ identifyUser = (req,result,res,myEmail,myPassword) => {
 }
 
 //identification user 
-exports.selectUserData = (req,res) => {
+exports.selectUserData = async(req,res) => {
   var myEmail = connection.escape(req.body.mail);
   var myPassword = connection.escape(req.body.mdp);
+  await bcrypt.hash(myPassword, 10).then(hash => {myPassword = hash})
   console.log(myPassword);
   connection.query({
-<<<<<<< HEAD
-      sql: `SELECT * FROM utilisateur WHERE mail = ${myEmail}`,
-=======
       sql: `SELECT mail, mdp, pseudo, statut FROM utilisateur WHERE mail = ${myEmail}`,
->>>>>>> e0d57dc23a4e08a9242c14432ede7d1bb7a5056b
       timeout: 10000}, 
       function (err, result) {
         if(err) throw err;
         console.log("je suis le premier résultat",result);
         identifyUser(req,result,res,myEmail,myPassword);
-        if (identifyUser(req,result,res,myEmail,myPassword) == true) {
+        if (identifyUser(req,result,myEmail,myPassword) == true) {
           req.session.userid = req.body.mail;
           res.send({ok: "ok",
-<<<<<<< HEAD
           result: result[0]});
-=======
-        result : result[0]});
->>>>>>> e0d57dc23a4e08a9242c14432ede7d1bb7a5056b
         }
         else {
           res.send({ok: "notok"});
         }
-        
       })
-}
+    }
 
 // création d'un utilisateur 
 exports.createUser = async(req,res) =>{
