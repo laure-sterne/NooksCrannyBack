2bc4013b8deddf10cdee1f217bb12b22936cc0e8
Vico4 <victoire.stahl@gmail.com>
2022-03-17 17:44:24 +0100

    decryptage du mdp hashé

--- controllers/utilisateurController.js
+++ controllers/utilisateurController.js
@@ -1,29 +1,44 @@
 const bcrypt = require('bcrypt')
 const connection = require('../connectionToBdd')
 
-identifyUser = (req,result,myEmail,myPassword) => {
+identifyUser = async (req, resu, result, myEmail, myPassword) => {
   console.log("Tentative de connexion");
-  console.log("je suis un résultat", result);
-  if(connection.escape(Object.values(result[0])[0])== myEmail && (connection.escape(Object.values(result[0])[1]) == myPassword)){
-    console.log(req.session, "User trouvé");
-    return true;
-  }
+  console.log("je suis un résultat", (Object.values(result[0])[1]));
+  var hash = Object.values(result[0])[1]
+  var verif = false 
+  await bcrypt.compare(myPassword, hash.toString(), 
+    function(err, res) {
+      if (err) {
+        console.log(err)
+      }
+      if (res) {
+        console.log("ok")
+        console.log(req.session, "User trouvé");
+        verif = true;
+        resu.send({ok: "ok",
+        result: result[0]});
+      } else {
+        console.log("non")
+        return false
+      }
+    })
+  console.log(verif)
+  return verif
 }
 
 //identification user 
 exports.selectUserData = async(req,res) => {
   var myEmail = connection.escape(req.body.mail);
   var myPassword = connection.escape(req.body.mdp);
-  await bcrypt.hash(myPassword, 10).then(hash => {myPassword = hash})
   console.log(myPassword);
   connection.query({
       sql: `SELECT mail, mdp, pseudo, statut FROM utilisateur WHERE mail = ${myEmail}`,
       timeout: 10000}, 
       function (err, result) {
         if(err) throw err;
-        console.log("je suis le premier résultat",result);
-        identifyUser(req,result,res,myEmail,myPassword);
-        if (identifyUser(req,result,myEmail,myPassword) == true) {
+        console.log("je suis le premier résultat", result);
+        identifyUser(req, res, result, myEmail, myPassword);
+        /* if (identifyUser(req, res, result,myEmail,myPassword) == true) {
           req.session.userid = req.body.mail;
           res.send({ok: "ok",
           result: result[0]});
@@ -31,8 +46,9 @@ exports.selectUserData = async(req,res) => {
         else {
           res.send({ok: "notok"});
         }
-      })
-    }
+      }) */
+    })
+  }
 
 // création d'un utilisateur 
 exports.createUser = async(req,res) =>{
